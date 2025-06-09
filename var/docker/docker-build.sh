c396792ae457b6f78341b59ff55e1c2c7f615a90
AASHRAYANKASETTY <59526137+AASHRAYANKASETTY@users.noreply.github.com>
2025-06-09 14:14:45 -0400

    Update docker-build.sh

--- var/docker/docker-build.sh
+++ var/docker/docker-build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 set -o xtrace
 
-# Clean up existing image
 docker rmi localhost/postiz || true
 
-# Build with explicit platform targeting AMD64
-docker build --platform linux/amd64 --target dist -t localhost/postiz -f Dockerfile.dev .
-docker build --platform linux/amd64 --target devcontainer -t localhost/postiz-devcontainer -f Dockerfile.dev .
+docker buildx build \
+  --platform linux/amd64 \
+  --target dist \
+  -t localhost/postiz \
+  -f Dockerfile.dev .
 
+docker buildx build \
+  --platform linux/amd64 \
+  --target devcontainer \
+  -t localhost/postiz-devcontainer \
+  -f Dockerfile.dev .
